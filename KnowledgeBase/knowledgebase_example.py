a902c5f83ae1d6df216fac8d224b708db3438f11
setsuna-217 <setsuna214722@gmail.com>
2024-09-20 09:45:51 +0800

    Deleted the input

--- KnowledgeBase/knowledgebase_example.py
+++ KnowledgeBase/knowledgebase_example.py
@@ -7,13 +7,13 @@ sfia = SFIA('sfiaskills.6.3.en.1.xlsx')
 # KnowledgeBase - processes the input from the client
 kb = KnowledgeBase( 'CSSE-allprograms-outcome-mappings-20240913.xlsx', sfia)
 
-for course, criterion in kb.criterionA.items():
-    print( course )
-    print( criterion.criterion_df )
-    print( "QA" )
-    print( criterion.criterion_qa_df )
+# for course, criterion in kb.criterionA.items():
+#     print( course )
+#     print( criterion.criterion_df )
+#     print( "QA" )
+#     print( criterion.criterion_qa_df )
     
-# for course, criterion in kb.criterionC.items():
-#         print(course)
-#         print(criterion.table_1_df)
-#         print(criterion.table_2_df)
+for course, criterion in kb.criterionC.items():
+        print(course)
+        print(criterion.table_1_df)
+        print(criterion.table_2_df)
