2a0c50e2eb012b404a707f6b05d071022c35232d
ShaluSeeni <shalu.seeni7@gmail.com>
2024-09-20 09:19:18 +0800

    Criterion E is generated with updated knowledge base

--- E_with_navi.py
+++ E_with_navi.py
@@ -6,7 +6,7 @@ from knowledgebase import KnowledgeBase
 sfia = SFIA('sfiaskills.6.3.en.1.xlsx')
 
 # KnowledgeBase - processes the input from the client
-kb = KnowledgeBase('CSSE-allprograms-outcome-mappings-20240821.xlsx', sfia)
+kb = KnowledgeBase('CSSE-allprograms-outcome-mappings-20240913.xlsx', sfia)
 
 # Prepare HTML content to save the output
 html_content = """
